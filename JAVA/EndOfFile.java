693db9753fcc1f1ec0af9b8ffeaf2157fa0757b1
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-03-01 23:53:29 +0530

    Create EndOfFile.java

---
+++ JAVA/EndOfFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<3;i++)
+        {
+            String s=sc.nextLine();
+            System.out.println(++count+" "+s);
+        
+        }
+    }
+}
