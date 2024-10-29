69dfe03d49e22b0b8d016475003511cd90a5eadc
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-10-29 23:30:45 +0530

    Update MergeStringsAlternately.java

--- JAVA/MergeStringsAlternately.java
+++ JAVA/MergeStringsAlternately.java
@@ -3,7 +3,6 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder mergedWord = new StringBuilder();
         int i = 0, j = 0;
-
         while (i < word1.length() && j < word2.length()) {
             mergedWord.append(word1.charAt(i));
             mergedWord.append(word2.charAt(j));
@@ -19,7 +18,6 @@ class Solution {
 
         return mergedWord.toString();
     }
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String word1 = sc.nextLine;
