bc1a7848b8bcbf24215948114d14d1822a626fb3
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-11-03 23:29:19 +0530

    Update MergeStringsAlternately.java

--- JAVA/MergeStringsAlternately.java
+++ JAVA/MergeStringsAlternately.java
@@ -17,6 +17,7 @@ class Solution {
         }
         return mergedWord.toString();
     }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String word1 = sc.nextLine;
